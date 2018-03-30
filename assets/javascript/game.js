23dd2fa3940335ac05896664e052ea3a029c64fa
Karina Solorio <macuser@Karinas-MacBook-Pro.local>
2018-03-30 13:58:06 -0400

    one step left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,51 @@
 
-//Comp Choices and variables
+//Computer's Choices
 var compChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+
+
+//Variables 
 var wins=0;
 var losses=0;
 var guessesLeft=9;
-var yourGuessesSoFar=0;
+var yourGuessesSoFar = [];
+var newGuess;
+
+//Computer Guess 
+var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
+
+//Function to update Computer guess after 9 tries or after a win    
+    //**************************//    
+
 
 // Function for key pressed
-// userGuess = key pressed
-// compGuess = random # which = random letter
 document.onkeyup = function(event){
-    var userGuess = event.key;
-    var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
-
-//Computer picks a random letter
-//Letter gets recorded each time
-
-//User guesses a letter 
-    if ((userGuess==="a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
+    //User's Guess
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();   
+    //User's Guess displayed on screen
+    yourGuessesSoFar.push(userGuess);
+    console.log(userGuess);
+        
+        //If user Wins
         if (userGuess === compGuess) {
+            alert("WOW YOU READ MY MIND!!");
             wins++;
             guessesLeft = 9;
-        } else {
-            guessesLeft--;
-
+            yourGuessesSoFar.length = 0;
+        } 
+        //If User uses up all of the guesses
+        else if (guessesLeft === 1) {
+            alert("I could have told you this was coming your way, but where's the fun in that?");
+            losses++;
+            guessesLeft=9;
+            yourGuessesSoFar.length = 0;
         }
-        
-     
+        //If User does not guess correct
+        else if (userGuess !== compGuess) {
+            guessesLeft--;
+            
+        }
 
-
-
-
+        //This will update whats in the HTML
         var html =
             "<h2>The Psychic Game</h2>" +
             "<p>Guess what letter I'm thinking of</p>" +
@@ -40,24 +55,9 @@ document.onkeyup = function(event){
             "<p>Your Guesses So Far: " + yourGuessesSoFar + "</p>";
           
 
-        // Set the inner HTML contents of the #game div to our html string
+        // Set the inner HTML contents of the #play div to our html string
             document.querySelector("#play").innerHTML = html;
 
     }
-};
-//Computer picks a random letter
-//Letter gets recorded each time
-
-//User guess compares to computer guess
-
-//User wrong then guesses goes down by 1
-
-    //after 9 tries losses goes up by 1
-
-    //guesses left restarts
-
-//user right then wins goes up by 1
-
-    //gueses left restarts
 
 
